10a99ed4edf1ca7daa73615cd33a819bc55cd9d2
Gustavo Soares de Souza <gu03soares@gmail.com>
2024-05-13 08:13:12 -0300

    Aulas de JS

--- EXERCÍCIOS/compartilhando/desconto/desconto.js
+++ EXERCÍCIOS/compartilhando/desconto/desconto.js
@@ -1,24 +1,24 @@
-let desconto = prompt("Digite um cupom de desconto de DESC1 à DESC5, o preço do seu produto é R$179.99")
+let desconto = prompt("Digite um cupom de desconto de DESC1 à DESC5, o preço do seu produto é R$100")
 
 switch (desconto) {
     case "DESC1":
-        alert(`DESC1 - 5% de desconto. Seu produto de R$179.00 agora está custando R$ ${179.99 - (5% 179.99)}`);
+        alert(`DESC1 - 5% de desconto. Seu produto de R$100 agora está custando R$${100 - (5% 100)}`);
         break;
 
     case "DESC2":
-        alert(`DESC2 - 10% de desconto. Seu produto de R$179.00 agora está custando R$ ${179.99 - (10% 179.99)}`);
+        alert(`DESC2 - 10% de desconto. Seu produto de R$100 agora está custando R$${100 - (10% 100)}`);
         break;
 
     case "DESC3":
-        alert(`DESC3 - 15% de desconto. Seu produto de R$179.00 agora está custando R$ ${179.99 - (15% 179.99)}`);
+        alert(`DESC3 - 15% de desconto. Seu produto de R$100 agora está custando R$${100 - (15% 100)}`);
         break;
 
     case "DESC4":
-        alert(`DESC4 - 20% de desconto. Seu produto de R$179.00 agora está custando R$ ${179.99 - (20% 179.99)}`);
+        alert(`DESC4 - 20% de desconto. Seu produto de R$100 agora está custando R$${100 - (20% 100)}`);
         break;
 
     case "DESC5":
-        alert(`DESC5 - 25% de desconto Seu produto de R$179.00 agora está custando R$ ${179.99 - (25% 179.99)}`);
+        alert(`DESC5 - 25% de desconto Seu produto de R$100 agora está custando R$${100 - (25% 100)}`);
         break;
 
     default:
