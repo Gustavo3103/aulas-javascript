66e4756c3f973d864b320fbf5bac6eb494dd6c74
Gustavo Soares de Souza <gu03soares@gmail.com>
2024-05-14 11:28:23 -0300

    Aula JS - Objetos

---
+++ JS/objetos.js
@@ -0,0 +1,18 @@
+let aluno1 = {
+    nome: "Gustavo",
+    curso: "Desenvolvimento WEB",
+    idade: 15,
+    nota: 10,
+    cursando: true
+}
+
+let aluno2 = {
+    nome: "Joãozinho",
+    curso: "Suporte em TI",
+    idade: 92,
+    nota: 3.5,
+    cursando: false
+}
+
+console.log(aluno1.nota);
+console.log(aluno2.nota);
